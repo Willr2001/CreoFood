521d226a196c8b28a99f44d71f9ddac17e29f38b
Mina Hanna <53411598+mhanna15@users.noreply.github.com>
2022-10-17 22:37:27 -0700

    small checkoff

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ingredients, setIngredients] = useState<string[]>([]);
+  const [input, setInput] = useState<string>('');
+
+  const fetchRecipes = () => {
+    const url = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=${ingredients.join(
+      ','
+    )}`;
+
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '261e394999msh8fb6322d521a783p1b272ejsned003eaab8aa',
+        'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+      },
+    };
+    fetch(url, options)
+      .then((res) => res.json())
+      .then((json) => console.log(json))
+      .catch((err) => console.error('error:' + err));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={input} onChange={(e) => setInput(e.target.value)} />
+      <button
+        onClick={() => {
+          setIngredients((oldIngredients) => [...oldIngredients, input]);
+          setInput('');
+        }}
+      >
+        Add ingredient
+      </button>
+      {ingredients}
+      <button onClick={fetchRecipes}>Get recipes</button>
     </div>
   );
 }
