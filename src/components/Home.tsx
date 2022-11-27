74ad226fe9b0e187cc78614693829be4440dbdb9
Will Ruskin <willjruskin@gmail.com>
2022-11-26 16:10:14 -0800

    long saved progress

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,6 +3,7 @@ import React, { Dispatch, SetStateAction, useState } from 'react';
 import { useAuth } from '../auth/AuthContext';
 import { db } from '../firebase';
 import { useNavigate } from 'react-router-dom';
+import './Home.css';
 
 const Home = (props: { setFavorites: Dispatch<SetStateAction<any>> }) => {
   const { currentUser, login, signup, loginWithGoogle, logout } = useAuth();
@@ -36,7 +37,72 @@ const Home = (props: { setFavorites: Dispatch<SetStateAction<any>> }) => {
 
   return currentUser ? (
     <div>
-      <button onClick={logout}>logout</button>
+      <header>
+        <img src="..\icons\recipeIcon.jpg" alt="receipeIcon"></img>
+        <h1>CreoFood</h1>
+      </header>
+
+      <div className="nav">
+        <a href="#section1" className="btn">
+          Meat
+        </a>
+        <a href="#section2" className="btn">
+          Veggies
+        </a>
+        <a href="#section3" className="btn">
+          Oils
+        </a>
+        <a href="#section4" className="btn">
+          Fish
+        </a>
+      </div>
+      <div className="section one" id="section1">
+        Meats<br></br>
+        <select name="meats" id="meats" multiple>
+          <option value="chicekn">Chicken</option>
+          <option value="lamb">Lamb</option>
+          <option value="beef">Beef</option>
+          <option value="bacon">Bacon</option>
+          <option value="pepperoni">pepperoni</option>
+        </select>
+      </div>
+      <div className="section two" id="section2">
+        Veggies<br></br>
+        <select name="veggies" id="veggies" multiple>
+          <option value="broccoli">Broccoli</option>
+          <option value="arugula">Arugula</option>
+          <option value="carrots">Carrorts</option>
+          <option value="celery">Celery</option>
+          <option value="dill">Dill</option>
+        </select>
+      </div>
+      <div className="section three" id="section3">
+        Oils<br></br>
+        <select name="oils" id="oils" multiple>
+          <option value="canoloa">canoloa</option>
+          <option value="vegetable">vegetable</option>
+          <option value="olive">olive</option>
+          <option value="avacado">avacado</option>
+        </select>
+      </div>
+      <div className="section four" id="section4">
+        Fish<br></br>
+        <select name="Fish" id="Fish" multiple>
+          <option value="salmon">Salmon</option>
+          <option value="cod">Cod</option>
+          <option value="lobster">Lobster</option>
+          <option value="crab">Crab</option>
+        </select>
+      </div>
+
+      <div className="sidenav">
+        <button id="logout-button" role="button" onClick={logout}>
+          logout
+        </button>
+        <a href="#about">About</a>
+        <a href="#Favourites">Favourite Recipes</a>
+      </div>
+
       <button onClick={() => navigate('/favorites')}>favorites</button>
       <input value={input} onChange={(e) => setInput(e.target.value)} />
       <button
